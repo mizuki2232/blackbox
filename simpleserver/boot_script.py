54fd8e92cbc5bae97507790500d98eb394b6fc30
Mizuki Kamata <mizuki255@gmail.com>
2017-08-03 10:51:36 +0000

    get attribute from json

--- simpleserver/boot_script.py
+++ simpleserver/boot_script.py
@@ -12,21 +12,6 @@ os.chdir(dir)
 PORT = 8000
 
 
-class MyHandler(SimpleHTTPRequestHandler):
-    def do_OPTIONS(self):
-        self.send_response(200, "ok")
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
-
-    def do_POST(self):
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.connection.shutdown(1)
-
-
 class CGIHandler(CGIHTTPRequestHandler):
     cgi_directories = ["/cgi-bin"]
 
--- simpleserver/cgi-bin/form.py
+++ simpleserver/cgi-bin/form.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 import cgi
 import cgitb
+import json
 
 
 # show error detail in client
 cgitb.enable()
+
+# load json
+config_json = open('json/config.json', 'r')
+config = json.load(config_json)
 
 
 print("Content-Type: text/html\n")
@@ -18,8 +23,14 @@ form = cgi.FieldStorage()
 if form.has_key("object"):
     print("<h1>What I'll watch is</h1><br>")
     print(form["object"].value)
-    print("<body></html>")
+    print("</body></html>")
 
 else:
     print("The Post request doesn't contain object")
-    print("<html><body>")
+
+
+for attr in config.get("list"):
+    if attr.get("id") == "Watch_Object":
+        print(attr.get('body'))
+
+print("</body></html>")
