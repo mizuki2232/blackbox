a0c5b46282bdea8f6acb33014fc87265c3ee4c46
Mizuki Kamata <mizuki255@gmail.com>
2017-08-03 19:20:07 +0000

    intaract with json

--- simpleserver/cgi-bin/form.py
+++ simpleserver/cgi-bin/form.py
@@ -20,8 +20,10 @@ print("<html><body>")
 form = cgi.FieldStorage()
 
 
+print("<h1>What I'll watch is</h1><br><hr>")
+
+
 if form.has_key("object"):
-    print("<h1>What I'll watch is</h1><br><hr>")
     for attr in config.get("list"):
         if attr.get("id") == "Watch_Object":
             attr["body"] = form["object"].value
@@ -35,7 +37,55 @@ else:
 
 
 
-# write config.json
+print("<br><br><h1>Desired Count</h1><br><hr>")
+
+
+if form.has_key("Count"):
+    for attr in config.get("list"):
+        if attr.get("id") == "Desired_Count":
+            attr["body"] = form["Count"].value
+            print("<h1>")
+            print(attr.get('body'))
+            print("</h1>")
+
+
+else:
+    print("<h1>" + "The Post request doesn't contain Desired Count" + "</h1>")
+
+
+print("<br><br><h1>Desired_Wait_Time</h1><br><hr>")
+
+
+if form.has_key("Wait_Time"):
+    for attr in config.get("list"):
+        if attr.get("id") == "Desired_Wait_Time":
+            attr["body"] = form["Wait_Time"].value
+            print("<h1>")
+            print(attr.get('body'))
+            print("</h1>")
+
+
+else:
+    print("<h1>" + "The Post request doesn't contain Desired Wait Time" + "</h1>")
+
+
+print("<br><br><h1>Mail Address</h1><br><hr>")
+
+
+if form.has_key("Mail_Address"):
+    for attr in config.get("list"):
+        if attr.get("id") == "Desired_Mail_Address":
+            attr["body"] = form["Mail_Address"].value
+            print("<h1>")
+            print(attr.get('body'))
+            print("</h1>")
+
+
+else:
+    print("<h1>" + "The Post request doesn't contain Mail Address" + "</h1>")
+
+
+# rewrite config.json
 config_json = open('json/config.json', 'w')
 json.dump(config, config_json, indent=4, sort_keys=True, separators=(',', ': '))
 
