121a1d69df33070b5fb549094558af4bbd5cb8ea
Mizuki Kamata <mizuki255@gmail.com>
2017-08-03 15:19:09 +0000

    add json write function

--- simpleserver/cgi-bin/form.py
+++ simpleserver/cgi-bin/form.py
@@ -21,16 +21,23 @@ form = cgi.FieldStorage()
 
 
 if form.has_key("object"):
-    print("<h1>What I'll watch is</h1><br>")
-    print(form["object"].value)
-    print("</body></html>")
+    print("<h1>What I'll watch is</h1><br><hr>")
+    for attr in config.get("list"):
+        if attr.get("id") == "Watch_Object":
+            attr["body"] = form["object"].value
+            print("<h1>")
+            print(attr.get('body'))
+            print("</h1>")
+
 
 else:
-    print("The Post request doesn't contain object")
+    print("<h1>" + "The Post request doesn't contain object" + "</h1>")
 
 
-for attr in config.get("list"):
-    if attr.get("id") == "Watch_Object":
-        print(attr.get('body'))
+
+# write config.json
+config_json = open('json/config.json', 'w')
+json.dump(config, config_json, indent=4, sort_keys=True, separators=(',', ': '))
+
 
 print("</body></html>")
