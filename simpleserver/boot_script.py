f1a34e2c38063467def000670e868972cfc1cd88
Mizuki Kamata <mizuki255@gmail.com>
2017-08-03 09:39:30 +0000

    enable cgi-script

--- simpleserver/boot_script.py
+++ simpleserver/boot_script.py
@@ -1,4 +1,4 @@
-import http.server
+from http.server import HTTPServer, CGIHTTPRequestHandler, SimpleHTTPRequestHandler
 import os
 import socketserver
 import sys
@@ -11,23 +11,26 @@ os.chdir(dir)
 
 PORT = 8000
 
-# Handler = http.server.SimpleHTTPRequestHandler
 
-class MyHandler(http.server.SimpleHTTPRequestHandler):
+class MyHandler(SimpleHTTPRequestHandler):
     def do_OPTIONS(self):
         self.send_response(200, "ok")
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
         self.send_header('Access-Control-Allow-Headers', 'X-Requested-With')
 
-    def do_GET(self):
+    def do_POST(self):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write('<html><body>Hello world!</body></html>')
         self.connection.shutdown(1)
 
-httpd = socketserver.TCPServer(("",PORT),MyHandler)
+
+class CGIHandler(CGIHTTPRequestHandler):
+    cgi_directories = ["/cgi-bin"]
+
+
+httpd = HTTPServer(("",PORT),CGIHandler)
 print("serving at port",PORT)
 httpd.serve_forever()
--- simpleserver/cgi-bin/form.py
+++ simpleserver/cgi-bin/form.py
@@ -1,34 +1,25 @@
 #!/usr/bin/env python
 import cgi
+import cgitb
 
 
-print "Content-Type: text/html\n"
-print "<html><body>"
+# show error detail in client
+cgitb.enable()
+
+
+print("Content-Type: text/html\n")
+print("")
+print("<html><body>")
 
 
 form = cgi.FieldStorage()
 
 
-class MyHandler(BaseHTTPRequestHandler):
-    def do_OPTIONS(self):           
-        self.send_response(200, "ok")       
-        self.send_header('Access-Control-Allow-Origin', '*')                
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")        
-
-    def do_GET(self):           
-        self.send_response(200)
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Content-type',    'text/html')                                    
-        self.end_headers()              
-        self.wfile.write("<html><body>Hello world!</body></html>")
-        self.connection.shutdown(1) 
-
-
 if form.has_key("object"):
-    print(form["name"].value)
-    MyHandler.do_OPTIONS()
-    MyHandler.do_GET()
+    print("<h1>What I'll watch is</h1><br>")
+    print(form["object"].value)
+    print("<body></html>")
 
-
-print "<body></html>"
+else:
+    print("The Post request doesn't contain object")
+    print("<html><body>")
